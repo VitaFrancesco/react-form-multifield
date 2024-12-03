4f2c854a05d46d2409c5946ab3d8418eaa8570b9
VitaFrancesco <vitafrancesco00@gmail.com>
2024-12-03 15:48:08 +0100

    ADD: useState with an object

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -3,47 +3,48 @@ import style from './AddBlog.module.css'
 
 export default function AddBlog({ onSubmit }) {
 
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
-    const [image, setImage] = useState(undefined)
-    const [published, setPublished] = useState(undefined)
-    const [tags, setTags] = useState([]);
+    const [post, setPost] = useState({
+        title: '',
+        content: '',
+        image: undefined,
+        tags: [],
+        published: true
+    })
 
 
     function articleSubmit(e, f) {
         e.preventDefault();
-        if (title === '') return;
         const newPost = {
-            id: Date.now(),
-            title: title,
-            image: image,
-            content: content,
-            tags: [],
-            published: published,
+            ...post,
+            id: Date.now()
         }
-        console.log(newPost)
-        setTitle('');
-        setContent('');
-        setImage(undefined);
-        setPublished(undefined)
         f(newPost);
+    }
+
+    function onChange(e) {
+        const value = e.target.type === "checkbox" ?
+            e.target.checked : e.target.value;
+        setPost({
+            ...post,
+            [e.target.name]: value
+        })
     }
 
 
     return (
         <div className='container'>
             <form className='form' onSubmit={(event) => articleSubmit(event, onSubmit)}>
-                <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} placeholder={`Titolo dell'articolo`} />
-                <select value={image} onChange={(e) => setImage(e.target.value)}>
-                    <option value={image}>Nessuna immagine</option>
+                <input type="text" name='title' onChange={(e) => onChange(e)} value={post.title} placeholder={`Titolo dell'articolo`} />
+                <select name='image' value={post.image} onChange={(e) => onChange(e)}>
+                    <option value={undefined}>Nessuna immagine</option>
                     <option value="../img/post2.jpg">Koeniggseg</option>
                     <option value="../img/post4.jpg">Panorama</option>
                     <option value="../img/post3.jpg">Romani</option>
                 </select>
                 <label htmlFor="select-image">Seleziona l'immagine</label>
-                <textarea className={style.textarea} placeholder='Scrivi il contenuto del post qui...' onChange={(e) => setContent(e.target.value)} value={content} ></textarea>
+                <textarea name='content' className={style.textarea} placeholder='Scrivi il contenuto del post qui...' onChange={(e) => onChange(e)} value={post.content} ></textarea>
                 <div className={style.dFlex}>
-                    <div className={style.tags}>
+                    {/* <div className={style.tags}>
                         <div>
                             <input type="checkbox" id='html' checked='html' onChange={(e) => setTags([...tags, e.target.checked])} />
                             <label htmlFor="html">html</label>
@@ -60,15 +61,15 @@ export default function AddBlog({ onSubmit }) {
                             <input type="checkbox" id='php' checked='php' onChange={(e) => setTags([...tags, e.target.checked])} />
                             <label htmlFor="php">php</label>
                         </div>
-                    </div>
-                    <div>
-                        <input type="radio" id="published-true" value={true} checked={published === true} onChange={() => setPublished(true)} />
+                    </div> */}
+                    {/* <div>
+                        <input type="radio" id="published-true" name='published' checked={true} onChange={(e) => onChange(e)} />
                         <label htmlFor="published-true">Pubblico</label>
                     </div>
                     <div>
-                        <input type="radio" id="published-false" value={false} checked={published === false} onChange={() => setPublished(false)} />
+                        <input type="radio" id="published-false" name='published' checked={false} onChange={(e) => onChange(e)} />
                         <label htmlFor="published-false">Privato</label>
-                    </div>
+                    </div> */}
                     <button type='submit'>Aggiungi</button>
                 </div>
             </form>
