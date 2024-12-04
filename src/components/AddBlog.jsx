ada54e8b1baf854c141593f5d6d6d569a46489e3
VitaFrancesco <vitafrancesco00@gmail.com>
2024-12-04 10:33:57 +0100

    ADD: bonus alert when change the value of publsihed

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,75 +1,104 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import style from './AddBlog.module.css'
 
 export default function AddBlog({ onSubmit }) {
-
-    const [post, setPost] = useState({
+    const initialPost = {
         title: '',
         content: '',
-        image: undefined,
+        image: "",
         tags: [],
         published: true
-    })
+    }
+
+    const [post, setPost] = useState(initialPost)
+    const [rendering, setRendering] = useState(1)
 
 
     function articleSubmit(e, f) {
         e.preventDefault();
+        if (post.title === '') return
         const newPost = {
             ...post,
             id: Date.now()
         }
+        setPost(initialPost)
         f(newPost);
     }
 
     function onChange(e) {
-        const value = e.target.type === "checkbox" ?
-            e.target.checked : e.target.value;
+
+        const elem = e.target;
+        let value;
+        if (elem.type === 'checkbox') {
+            value = elem.checked;
+        } if (elem.type === 'radio') {
+            value = JSON.parse(elem.value);
+        } else {
+            value = elem.value;
+        }
+
+        console.log([e.target, e.target.type, e.target.checked]);
         setPost({
             ...post,
-            [e.target.name]: value
+            [elem.name]: value
         })
+        console.log(post)
     }
+
+    function manageTags(tag) {
+        setPost({
+            ...post,
+            tags: post.tags.includes(tag)
+                ? post.tags.filter(t => t !== tag)
+                : [...post.tags, tag]
+        });
+    }
+
+    useEffect(() => {
+        rendering ? setRendering(0) :
+            alert(`Stai creando un post ${post.published ? 'publico' : 'privato'}`)
+    }, [post.published])
 
 
     return (
         <div className='container'>
             <form className='form' onSubmit={(event) => articleSubmit(event, onSubmit)}>
                 <input type="text" name='title' onChange={(e) => onChange(e)} value={post.title} placeholder={`Titolo dell'articolo`} />
-                <select name='image' value={post.image} onChange={(e) => onChange(e)}>
-                    <option value={undefined}>Nessuna immagine</option>
-                    <option value="../img/post2.jpg">Koeniggseg</option>
-                    <option value="../img/post4.jpg">Panorama</option>
-                    <option value="../img/post3.jpg">Romani</option>
+                <select name="image" value={post.image} onChange={(e) => onChange(e)}>
+                    <option value="">Nessuna immagine</option>
+                    <option value="/img/post2.jpg">Koeniggseg</option>
+                    <option value="/img/post4.jpg">Panorama</option>
+                    <option value="/img/post3.jpg">Romani</option>
                 </select>
                 <label htmlFor="select-image">Seleziona l'immagine</label>
                 <textarea name='content' className={style.textarea} placeholder='Scrivi il contenuto del post qui...' onChange={(e) => onChange(e)} value={post.content} ></textarea>
                 <div className={style.dFlex}>
-                    {/* <div className={style.tags}>
+                    <div className={style.tags}>
                         <div>
-                            <input type="checkbox" id='html' checked='html' onChange={(e) => setTags([...tags, e.target.checked])} />
+                            <input type="checkbox" id='html' checked={post.tags.includes('html')} name='html' onChange={(e) => manageTags(e.target.name)} />
                             <label htmlFor="html">html</label>
                         </div>
                         <div>
-                            <input type="checkbox" id='css' checked='css' onChange={(e) => setTags([...tags, e.target.checked])} />
+                            <input type="checkbox" id='css' checked={post.tags.includes('css')} name='css' onChange={(e) => manageTags(e.target.name)} />
                             <label htmlFor="css">css</label>
                         </div>
                         <div>
-                            <input type="checkbox" id='js' checked='js' onChange={(e) => setTags([...tags, e.target.checked])} />
+                            <input type="checkbox" id='js' checked={post.tags.includes('js')} name='js' onChange={(e) => manageTags(e.target.name)} />
                             <label htmlFor="js">js</label>
                         </div>
                         <div>
-                            <input type="checkbox" id='php' checked='php' onChange={(e) => setTags([...tags, e.target.checked])} />
+                            <input type="checkbox" id='php' checked={post.tags.includes('php')} name='php' onChange={(e) => manageTags(e.target.name)} />
                             <label htmlFor="php">php</label>
                         </div>
-                    </div> */}
-                    {/* <div>
-                        <input type="radio" id="published-true" name='published' checked={true} onChange={(e) => onChange(e)} />
+                    </div>
+                    <div>
+                        <input type="radio" id="published-true" name="published" value={true} checked={post.published} onChange={(e) => onChange(e)} />
                         <label htmlFor="published-true">Pubblico</label>
                     </div>
                     <div>
-                        <input type="radio" id="published-false" name='published' checked={false} onChange={(e) => onChange(e)} />
+                        <input type="radio" id="published-false" name="published" value={false} checked={!post.published} onChange={(e) => onChange(e)} />
                         <label htmlFor="published-false">Privato</label>
-                    </div> */}
+                    </div>
                     <button type='submit'>Aggiungi</button>
                 </div>
             </form>
